d81d5e5837db28a1d360fc4128cf41c63fffe08f
meszkos <michalik515@gmail.com>
2022-06-27 13:30:54 +0200

    Initial Commit

---
+++ SpotifySearchTab/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpotifySearchTab
+//
+//  Created by MacOS on 27/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpotifySearchTab/SpotifySearchTabApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifySearchTabApp.swift
+//  SpotifySearchTab
+//
+//  Created by MacOS on 27/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct SpotifySearchTabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
