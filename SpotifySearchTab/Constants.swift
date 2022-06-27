b53f620074cdaf3e6fa4e3d9d0daee5b56ccf3cd
meszkos <michalik515@gmail.com>
2022-06-27 14:02:10 +0200

    Add Colors

---
+++ SpotifySearchTab/Constants.swift
@@ -0,0 +1,31 @@
+//
+//  Constants.swift
+//  SpotifySearchTab
+//
+//  Created by MacOS on 27/06/2022.
+//
+
+import Foundation
+import SwiftUI
+
+struct K{
+    
+    static let cellColors = [
+        Color.color1,
+        Color.color2,
+        Color.color3,
+        Color.color4,
+        Color.color5,
+        Color.color6,
+        Color.color7,
+        Color.color8,
+        Color.color9,
+        Color.color10,
+        Color.color11,
+        Color.color12,
+        Color.color13,
+        Color.color14,
+        Color.color15
+    ]
+    
+}
---
+++ SpotifySearchTab/Extentions.swift
@@ -0,0 +1,26 @@
+//
+//  Extentions.swift
+//  SpotifySearchTab
+//
+//  Created by MacOS on 27/06/2022.
+//
+
+import SwiftUI
+
+extension Color{
+    static let color1 = Color("Color-1")
+    static let color2 = Color("Color-2")
+    static let color3 = Color("Color-3")
+    static let color4 = Color("Color-4")
+    static let color5 = Color("Color-5")
+    static let color6 = Color("Color-6")
+    static let color7 = Color("Color-7")
+    static let color8 = Color("Color-8")
+    static let color9 = Color("Color-9")
+    static let color10 = Color("Color-10")
+    static let color11 = Color("Color-11")
+    static let color12 = Color("Color-12")
+    static let color13 = Color("Color-13")
+    static let color14 = Color("Color-14")
+    static let color15 = Color("Color-15")
+}
